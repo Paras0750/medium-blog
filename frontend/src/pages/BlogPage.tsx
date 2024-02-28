62be1eb1f9b34efa2a4f5bfff12ad8062192879b
Paras0750 <nauriyalparas0@gmail.com>
2024-02-29 04:59:49 +0530

    Added Vercel config

--- frontend/src/pages/BlogPage.tsx
+++ frontend/src/pages/BlogPage.tsx
@@ -1,14 +1,15 @@
 import { useGetAllBlogsQuery } from "@/service/blogService";
-
+import { useNavigate } from "react-router-dom";
 
 export const BlogPage = () => {
-  const { data, isLoading, isError } =  useGetAllBlogsQuery();
-
+  const { data, isLoading, isError } = useGetAllBlogsQuery();
+  const navigate = useNavigate();
   if (isLoading) return <div>Loading...</div>;
   console.log(data);
 
   if (isError || !data) return <div>Error</div>;
 
+  if (localStorage.getItem("token") === null) navigate("/signin");
   return (
     <div>
       Welcome To Blog Page
